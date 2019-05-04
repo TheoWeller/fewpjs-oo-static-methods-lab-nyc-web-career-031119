01c73d916b42326d60178f71331befe601d5ba13
Theo Weller <weller.theodor@gmail.com>
2019-05-04 18:11:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    string.charAt(0).toUpperCase();
+    return string
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-']+/g, ' ')
+  }
+
+  static titleize(string){
+    string.charAt(0).toUpperCase();
+    let splitString = string.split(" ");
+
+    let newString = splitString.forEach(word => {
+      if(word === "the" || word === "a" || word === "an" || word === "but" || word === "of" || word === "and" || word === "for" || word === "at" || word === "by" || word === "from"){
+
+      } else {
+        word.charAt(0).toUpperCase();
+      }
+
+
+    })
+    newString.join("")
+    return newString
+  }
 }
