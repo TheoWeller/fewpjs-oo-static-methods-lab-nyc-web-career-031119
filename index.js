1b2d27f6c1cb5e84711e7c616c1afa0ce9384d06
Theo Weller <weller.theodor@gmail.com>
2019-05-04 18:12:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 class Formatter {
   //add static methods here
   static capitalize(string){
-    string.charAt(0).toUpperCase();
+    string = string.charAt(0).toUpperCase() + string.slice(1);
     return string
   }
 
